b6b4bf4191489f4e5b70c5cc3a635abcb40c9818
Koushlendra Singh Parihar <skoushlendra@gmail.com>
2015-12-22 16:29:24 +0530

    mean setup
    connection with mongolab

---
+++ server.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log("start");
+
+var config = require('./config/config'),
+	mongoose = require('./config/mongoose'),
+	express = require('./config/express'),
+	passport = require('./config/passport');
+
+var db = mongoose(),
+	app = express(),
+	passport = passport();
+
+app.listen(config.port);
+
+module.exports = app;
+console.log("skoushlendra");
+console.log(process.env.NODE_ENV + ' server running at http://mean-mvc.heroku.com:' + config.port);
