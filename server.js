6f2e7416a24c6f29ddbc483468c5815f627a11d2
Koushlendra Singh Parihar <skoushlendra@gmail.com>
2015-12-22 17:23:10 +0530

    latest
    latest

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ var db = mongoose(),
 	app = express(),
 	passport = passport();
 
-//app.listen((process.env. || 5000));
+app.listen((process.env.PORT || 5000));
 console.log("process.env.PORT: "+process.env.PORT);
-app.listen(process.env.PORT || 5000, function(){
+/*app.listen(process.env.PORT || 5000, function(){
    var host = app.address().address;
    var port = app.address().port;
    console.log('Listening on http://%s:%s', host, port);
 });
+*/
 
 module.exports = app;
 console.log("skoushlendra");
